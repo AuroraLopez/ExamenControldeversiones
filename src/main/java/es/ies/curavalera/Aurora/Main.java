1a12c811748f0456b4ac50f678ad9a65915b79d1
AuroraLopez <alopfue679@g.educaand.es>
2026-04-16 10:43:33 +0200

    Trabaja refactorizado

---
+++ src/main/java/es/ies/curavalera/Aurora/Main.java
@@ -0,0 +1,13 @@
+﻿package es.ies.curavalera.Aurora;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("44885522R","Juan Gómez");
+        empleado1.setDireccion("C/de mi casa,5");
+        empleado1.setNumHijos(12);
+
+        empleado1.infoEmpleado();
+    }
+    
+}
